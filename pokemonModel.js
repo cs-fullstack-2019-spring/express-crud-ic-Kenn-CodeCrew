67ac0bae26bdd77ca0eafc01fbcaf20109e74f53
Kenn-CodeCrew <40698252+Kenn-CodeCrew@users.noreply.github.com>
2019-03-24 21:03:02 -0500

    Add files via upload

---
+++ pokemonModel.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+
+var PokemonSchema = new Schema(
+    {
+        id: Number,
+        name: String,
+        username: String,
+        email: String,
+        address:[{
+            street: String,
+            suite: String,
+            city: String,
+            zipcode: String,
+            geo:[{
+                lat: Number,
+                lng: Number,
+            }],
+        }],
+        phone: String,
+        website: String,
+        company: [{
+            name: String,
+            catchPhrase: String,
+            bs: String,
+        }],
+    });
+
+
+//Export model
+module.exports = mongoose.model('Pokemon', PokemonSchema);
